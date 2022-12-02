673c90511deab10c5ea51db8f33a0e5322405e01
abhij <abhij@SHARVIL-B>
2022-12-02 16:56:51 +0530

    Added Print Statements

--- testGitProject/src/gitPackage/A.java
+++ testGitProject/src/gitPackage/A.java
@@ -6,6 +6,10 @@ public class A {
 		System.out.println("Hello");
 		System.out.println("Hello Automation");
 		System.out.println("Hello Automation1");
+		System.out.println("Hello lots");
+		System.out.println("Hello preeti");
+		System.out.println("Hello Sharvil");
+	
 	}
 
 }
